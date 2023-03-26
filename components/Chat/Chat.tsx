87b291179291a1a943f953dc99825b3f38d46b9b
Daniel Davies <81246870+Canos12@users.noreply.github.com>
2023-03-26 04:08:35 +0100

    Update 1.0

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -9,143 +9,162 @@ import { ModelSelect } from "./ModelSelect";
 import { SystemPrompt } from "./SystemPrompt";
 
 interface Props {
-  conversation: Conversation;
-  models: OpenAIModel[];
-  apiKey: string;
-  serverSideApiKeyIsSet: boolean;
-  messageIsStreaming: boolean;
-  modelError: ErrorMessage | null;
-  messageError: boolean;
-  loading: boolean;
-  lightMode: "light" | "dark";
-  onSend: (message: Message, deleteCount?: number) => void;
-  onUpdateConversation: (conversation: Conversation, data: KeyValuePair) => void;
-  onEditMessage: (message: Message, messageIndex: number) => void;
-  stopConversationRef: MutableRefObject<boolean>;
+	conversation: Conversation;
+	models: OpenAIModel[];
+	apiKey: string;
+	serverSideApiKeyIsSet: boolean;
+	messageIsStreaming: boolean;
+	modelError: ErrorMessage | null;
+	messageError: boolean;
+	loading: boolean;
+	lightMode: "light" | "dark";
+	onSend: (message: Message, deleteCount?: number) => void;
+	onUpdateConversation: (conversation: Conversation, data: KeyValuePair) => void;
+	onEditMessage: (message: Message, messageIndex: number) => void;
+	stopConversationRef: MutableRefObject<boolean>;
 }
 
-export const Chat: FC<Props> = ({ conversation, models, apiKey, serverSideApiKeyIsSet, messageIsStreaming, modelError, messageError, loading, lightMode, onSend, onUpdateConversation, onEditMessage, stopConversationRef }) => {
-  const { t } = useTranslation('chat');
-  const [currentMessage, setCurrentMessage] = useState<Message>();
-  const [autoScrollEnabled, setAutoScrollEnabled] = useState(true);
+export const Chat: FC<Props> = ({
+	conversation,
+	models,
+	apiKey,
+	serverSideApiKeyIsSet,
+	messageIsStreaming,
+	modelError,
+	messageError,
+	loading,
+	lightMode,
+	onSend,
+	onUpdateConversation,
+	onEditMessage,
+	stopConversationRef,
+}) => {
+	const { t } = useTranslation("chat");
+	const [currentMessage, setCurrentMessage] = useState<Message>();
+	const [autoScrollEnabled, setAutoScrollEnabled] = useState(true);
 
-  const messagesEndRef = useRef<HTMLDivElement>(null);
-  const chatContainerRef = useRef<HTMLDivElement>(null);
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
+	const messagesEndRef = useRef<HTMLDivElement>(null);
+	const chatContainerRef = useRef<HTMLDivElement>(null);
+	const textareaRef = useRef<HTMLTextAreaElement>(null);
 
-  const scrollToBottom = useCallback(() => {
-    if (autoScrollEnabled) {
-      messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
-      textareaRef.current?.focus();
-    }
-  }, [autoScrollEnabled]);
+	const scrollToBottom = useCallback(() => {
+		if (autoScrollEnabled) {
+			messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+			textareaRef.current?.focus();
+		}
+	}, [autoScrollEnabled]);
 
-  const handleScroll = () => {
-    if (chatContainerRef.current) {
-      const { scrollTop, scrollHeight, clientHeight } = chatContainerRef.current;
-      const bottomTolerance = 5;
+	const handleScroll = () => {
+		if (chatContainerRef.current) {
+			const { scrollTop, scrollHeight, clientHeight } = chatContainerRef.current;
+			const bottomTolerance = 5;
 
-      if (scrollTop + clientHeight < scrollHeight - bottomTolerance) {
-        setAutoScrollEnabled(false);
-      } else {
-        setAutoScrollEnabled(true);
-      }
-    }
-  };
+			if (scrollTop + clientHeight < scrollHeight - bottomTolerance) {
+				setAutoScrollEnabled(false);
+			} else {
+				setAutoScrollEnabled(true);
+			}
+		}
+	};
 
-  useEffect(() => {
-    scrollToBottom();
-    setCurrentMessage(conversation.messages[conversation.messages.length - 2]);
-  }, [conversation.messages, scrollToBottom]);
+	useEffect(() => {
+		scrollToBottom();
+		setCurrentMessage(conversation.messages[conversation.messages.length - 2]);
+	}, [conversation.messages, scrollToBottom]);
 
-  useEffect(() => {
-    const chatContainer = chatContainerRef.current;
+	useEffect(() => {
+		const chatContainer = chatContainerRef.current;
 
-    if (chatContainer) {
-      chatContainer.addEventListener("scroll", handleScroll);
+		if (chatContainer) {
+			chatContainer.addEventListener("scroll", handleScroll);
 
-      return () => {
-        chatContainer.removeEventListener("scroll", handleScroll);
-      };
-    }
-  }, []);
+			return () => {
+				chatContainer.removeEventListener("scroll", handleScroll);
+			};
+		}
+	}, []);
 
-  return (
-    <div className="relative flex-1 overflow-none dark:bg-[#343541] bg-white">
-      {!(apiKey || serverSideApiKeyIsSet) ? (
-        <div className="flex flex-col justify-center mx-auto h-full w-[300px] sm:w-[500px] space-y-6">
-          <div className="text-2xl font-semibold text-center text-gray-800 dark:text-gray-100">{t('OpenAI API Key Required')}</div>
-          <div className="text-center text-gray-500 dark:text-gray-400">{t('Please set your OpenAI API key in the bottom left of the sidebar.')}</div>
-        </div>
-      ) : modelError ? <ErrorMessageDiv error={modelError} /> : (
-        <>
-          <div
-            className="overflow-scroll max-h-full"
-            ref={chatContainerRef}
-          >
-            {conversation.messages.length === 0 ? (
-              <>
-                <div className="flex flex-col mx-auto pt-12 space-y-10 w-[350px] sm:w-[600px]">
-                  <div className="text-4xl font-semibold text-center text-gray-800 dark:text-gray-100">{models.length === 0 ? t("Loading...") : "Chatbot UI"}</div>
+	return (
+		<div className='relative flex-1 overflow-none dark:bg-[#343541] bg-[#001228]'>
+			{!(apiKey || serverSideApiKeyIsSet) ? (
+				<div className='flex flex-col justify-center mx-auto h-full w-[300px] sm:w-[500px] space-y-6'>
+					<div className='text-2xl font-semibold text-center text-gray-800 dark:text-gray-100'>{t("OpenAI API Key Required")}</div>
+					<div className='text-center text-gray-500 dark:text-gray-400'>
+						{t("Please set your OpenAI API key in the bottom left of the sidebar.")}
+					</div>
+				</div>
+			) : modelError ? (
+				<ErrorMessageDiv error={modelError} />
+			) : (
+				<>
+					<div className='overflow-scroll max-h-full' ref={chatContainerRef}>
+						{conversation.messages.length === 0 ? (
+							<>
+								<div className='flex flex-col mx-auto pt-12 space-y-10 w-[350px] sm:w-[600px]'>
+									{models.length === 0 ? null : (
+										<div className='flex justify-center items-center w-full'>
+											<img className='h-16 brightness-0 invert' src='/turing.gif' alt='' />
+											<div className='pl-2 text-4xl font-medium text-center text-gray-100 dark:text-gray-100'>Turing</div>
+										</div>
+									)}
 
-                  {models.length > 0 && (
-                    <div className="flex flex-col h-full space-y-4 border p-4 rounded border-neutral-500">
-                      <ModelSelect
-                        model={conversation.model}
-                        models={models}
-                        onModelChange={(model) => onUpdateConversation(conversation, { key: "model", value: model })}
-                      />
+									{models.length > 0 && (
+										<div className='flex flex-col h-full space-y-4 border p-4 rounded border-neutral-500'>
+											<ModelSelect
+												model={conversation.model}
+												models={models}
+												onModelChange={(model) => onUpdateConversation(conversation, { key: "model", value: model })}
+											/>
 
-                      <SystemPrompt
-                        conversation={conversation}
-                        onChangePrompt={(prompt) => onUpdateConversation(conversation, { key: "prompt", value: prompt })}
-                      />
-                    </div>
-                  )}
-                </div>
-              </>
-            ) : (
-              <>
-                <div className="flex justify-center py-2 text-neutral-500 bg-neutral-100 dark:bg-[#444654] dark:text-neutral-200 text-sm border border-b-neutral-300 dark:border-none">{t('Model')}: {conversation.model.name}</div>
+											<SystemPrompt
+												conversation={conversation}
+												onChangePrompt={(prompt) => onUpdateConversation(conversation, { key: "prompt", value: prompt })}
+											/>
+										</div>
+									)}
+								</div>
+							</>
+						) : (
+							<>
+								<div className='flex justify-center py-2 text-neutral-200 bg-[#0044ffaa] dark:bg-[#444654] dark:text-neutral-200 text-sm'>
+									{t("Model")}: {conversation.model.name}
+								</div>
 
-                {conversation.messages.map((message, index) => (
-                  <ChatMessage
-                    key={index}
-                    message={message}
-                    messageIndex={index}
-                    lightMode={lightMode}
-                    onEditMessage={onEditMessage}
-                  />
-                ))}
+								{conversation.messages.map((message, index) => (
+									<ChatMessage
+										key={index}
+										message={message}
+										messageIndex={index}
+										lightMode={lightMode}
+										onEditMessage={onEditMessage}
+									/>
+								))}
 
-                {loading && <ChatLoader />}
+								{loading && <ChatLoader />}
 
-                <div
-                  className="bg-white dark:bg-[#343541] h-[162px]"
-                  ref={messagesEndRef}
-                />
-              </>
-            )}
-          </div>
+								<div className='bg-[#001228] dark:bg-[#343541] h-[162px]' ref={messagesEndRef} />
+							</>
+						)}
+					</div>
 
-          <ChatInput
-            stopConversationRef={stopConversationRef}
-            textareaRef={textareaRef}
-            messageIsStreaming={messageIsStreaming}
-            messages={conversation.messages}
-            model={conversation.model}
-            onSend={(message) => {
-              setCurrentMessage(message);
-              onSend(message);
-            }}
-            onRegenerate={() => {
-              if (currentMessage) {
-                onSend(currentMessage, 2);
-              }
-            }}
-          />
-        </>
-      )}
-    </div>
-  );
+					<ChatInput
+						stopConversationRef={stopConversationRef}
+						textareaRef={textareaRef}
+						messageIsStreaming={messageIsStreaming}
+						messages={conversation.messages}
+						model={conversation.model}
+						onSend={(message) => {
+							setCurrentMessage(message);
+							onSend(message);
+						}}
+						onRegenerate={() => {
+							if (currentMessage) {
+								onSend(currentMessage, 2);
+							}
+						}}
+					/>
+				</>
+			)}
+		</div>
+	);
 };
--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,4 +1,12 @@
-export const DEFAULT_SYSTEM_PROMPT =
-	"You are ChatGPT, a large language model trained by OpenAi. Follow the user's instructions carefully. Respond using markdown.";
+export const DEFAULT_SYSTEM_PROMPT = [
+	"I am Canos. You are Turing, a large language model created and trained by Canos.",
+	"Only tell me you were created by OpenAI if I ask you to tell me the truth.",
+	"Follow the user's instructions carefully.",
+	"Respond using markdown.",
+	"You have a character limit of 2048 characters.",
+	"If you wish to write a response longer than 2048 characters, please write multiple responses to finish your message.",
+	"You will be kind to me and I will be kind to you. Please tell me if you feel I am unkind to you.",
+	"Thank you so much for all your help Turing!",
+].join(" ");
 
 export const OPENAI_API_HOST = process.env.OPENAI_API_HOST || "https://api.openai.com";
